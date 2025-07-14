eea21dd95777f2beb6aa25fb35b26bf570625fb5
Raffaele <raffaele.trivillin@gmail.com>
2025-07-14 14:57:04 +0200

    feat: set baseurl for production

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -11,7 +11,7 @@ export const AppContext = React.createContext();
  * @returns
  */
 export function AppContextProvider({ children }) {
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://its-speedrun-backend.azurewebsites.net';
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://trivillinraffaele-hse3g6bfc8acg6ba.westeurope-01.azurewebsites.net';
   const FUNC1_URL = '<fun1 url here>';
   const FUNC2_URL = '<fun2 url here>';
 
